668d83bac964ba7b183ba941843e44b912fb0901
jogo <jogo@syntronic.com>
2018-11-15 14:02:11 +0100

    2 Tests added

---
+++ PerformLogSearchLogicTest.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Generic;
+using Ericsson.LogAnalyzer;
+using Ericsson.LogAnalyzer.Models;
+using NSubstitute;
+using NUnit.Framework;
+using System.IO;
+using Ericsson.LogAnalyzer.Definitions;
+using TestUtilities;
+using Ericsson.LogAnalyzer.BLL;
+using Ericsson.LogAnalyzer.Models.Entity;
+
+namespace LogAnalyzerTest
+{
+    [TestFixture]
+    public class PerformLogSearchLogicTest
+    {
+        // R-states aren't equal
+        public readonly static ChooseCase UnEqualRStateCase = new ChooseCase(rstate: "ee");
+        // Invalid R-states are equal
+        public readonly static ChooseCase EqualExcludedRStateCase = new ChooseCase(excludedrstate: "RSA", rstate: null);
+        // Invalid R-states aren't equal
+        public readonly static ChooseCase UnEqualExcludedRStateCase = new ChooseCase(excludedrstate: "ee", rstate: null);
+        // Product numbers aren't equal
+        public readonly static ChooseCase UnEqualProdNumCase = new ChooseCase(productnumber: "KDU 127 925/31");
+        // Date isn't in range of product manufacturing date
+        public readonly static ChooseCase DateNotInRangeCase = new ChooseCase(datefrom: "20170201", dateto: "20170301");
+        // Date is in range of product manufacturing date
+        public readonly static ChooseCase DateInRangeCase = new ChooseCase(datefrom: "20171201", dateto: "20180301");
+        // Station type is not an included station type
+        public readonly static ChooseCase UnEqualStationTypeCase = 
+                               new ChooseCase(includetesttypes: new List<StationTestType> { StationTestType.CloudLat });
+        // Excluded station types are equal
+        public readonly static ChooseCase EqualExcludedStationTypeCase = 
+                               new ChooseCase(excludetesttypes: new List<StationTestType> { StationTestType.RcPrtt });
+        // Excluded station types aren't equal
+        public readonly static ChooseCase UnEqualExcludedStationTypeCase = 
+                               new ChooseCase(excludetesttypes: new List<StationTestType> { StationTestType.CloudLat });
+
+        ChooseCase CurrentCase { get; set; }
+
+        private string HwLogCriteria { get; } = Path.Combine(FilePaths.RepoRoot, @"Datapackets\HWLogCriteria\documents\HwLogCriteria.xml");
+
+        [Test]
+        [TestCase("UnEqualRStateCase", ExpectedResult = false)]
+        [TestCase("EqualExcludedRStateCase", ExpectedResult = false)]
+        [TestCase("UnEqualExcludedRStateCase", ExpectedResult = true)]
+        [TestCase("UnEqualProdNumCase", ExpectedResult = false)]
+        [TestCase("DateNotInRangeCase", ExpectedResult = false)]
+        [TestCase("DateInRangeCase", ExpectedResult = true)]
+        [TestCase("UnEqualStationTypeCase", ExpectedResult = false)]
+        [TestCase("EqualExcludedStationTypeCase", ExpectedResult = false)]
+        [TestCase("UnEqualExcludedStationTypeCase", ExpectedResult = true)]
+        public bool ShallSearchBePerformedTest(string Key)
+        {
+            //Arrange
+            CurrentCase = GetObject(Key);
+            ProductModel currentProduct = GetProduct("KDU 137 925/31");
+
+            LogSearchLogic logic = new LogSearchLogic(DatabaseVariantType.Xml,
+                                                      HwLogCriteria,
+                                                      GetCustomFunctions(currentProduct));
+
+            var key = logic.GetSearchKeyByName("CRAMC_error", "Baseband");
+
+            key.SearchSettings.IncludeTestTypes = CurrentCase.IncludeTestTypes;
+            key.SearchSettings.ExcludeTestTypes = CurrentCase.ExcludeTestTypes;
+
+            key.SearchSettings.Products = GetProductEntityList();
+            //Act
+            var Result = key.ShallSearchBePerformed(currentProduct, CurrentCase.TestType,
+                                                    CurrentCase.ServiceLoction);
+            //Assert
+            return Result;
+        }
+
+        private static ChooseCase GetObject(string key)
+        {
+            return key == "UnEqualRStateCase" ? UnEqualRStateCase :
+                   key == "EqualExcludedRStateCase" ? EqualExcludedRStateCase :
+                   key == "UnEqualExcludedRStateCase" ? UnEqualExcludedRStateCase :
+                   key == "UnEqualProdNumCase" ? UnEqualProdNumCase :
+                   key == "DateNotInRangeCase" ? DateNotInRangeCase :
+                   key == "DateInRangeCase" ? DateInRangeCase :
+                   key == "UnEqualStationTypeCase" ? UnEqualStationTypeCase :
+                   key == "EqualExcludedStationTypeCase" ? EqualExcludedStationTypeCase :
+                   key == "UnEqualExcludedStationTypeCase" ? UnEqualExcludedStationTypeCase :
+                   null;
+        }
+
+        CustomFunctions GetCustomFunctions(ProductModel product)
+        {
+            return new CustomFunctions(null, product, Substitute.For<ILogger>());
+        }
+
+        private static ProductModel GetProduct(string productNumber)
+        {
+            ProductModel currentProduct = Substitute.For<ProductModel>();            
+            currentProduct.ProductNumber = "KDU 137 925/31";
+            currentProduct.RState = "RSA";
+            currentProduct.ProductDate = "20180101";
+            return currentProduct;
+        }
+
+        private List<ProductEntity> GetProductEntityList()
+        {
+
+            var ProductToCompare = Substitute.For<ProductModel>();
+            {
+                ProductToCompare.SerialNumber = CurrentCase.Serialnumber;
+                ProductToCompare.ProductNumber = CurrentCase.ProductNumber;
+                ProductToCompare.RState = CurrentCase.RState;
+                ProductToCompare.ExcludedRState = CurrentCase.ExcludedRState;
+                ProductToCompare.ProductDate = CurrentCase.ProductDate;
+                ProductToCompare.DateFrom = CurrentCase.DateFrom;
+                ProductToCompare.DateTo = CurrentCase.DateTo;
+            }
+            var ProductToCompareList = new List<ProductEntity>()
+            {
+                ProductToCompare
+            };
+
+            return ProductToCompareList;
+        }
+
+
+    }
+
+    public class ChooseCase
+    {
+
+        public StationTestType TestType { get; set; }
+
+        public List<StationTestType> IncludeTestTypes { get; set; }
+
+        public List<StationTestType> ExcludeTestTypes { get; set; }
+
+        public string ServiceLoction { get; set; }
+
+        public string Serialnumber { get; set; }
+
+        public string ProductNumber { get; set; }
+
+        public string RState { get; set; }
+
+        public string ExcludedRState { get; set; }
+
+        public string ProductDate { get; set; }
+
+        public string DateFrom { get; set; }
+
+        public string DateTo { get; set; }
+
+        public ChooseCase(StationTestType testtype = StationTestType.RcPrtt,
+                          List<StationTestType> includetesttypes = null,
+                          List<StationTestType> excludetesttypes = null,
+                          string serviceLocation = null,
+                          string serialnumber = "*",
+                          string productnumber = "KDU 137 925/31",
+                          string rstate = "RSA",
+                          string excludedrstate = null,
+                          string productdate = null,
+                          string datefrom = null,
+                          string dateto = null)
+        {
+            TestType = testtype;
+            IncludeTestTypes = includetesttypes ?? new List<StationTestType> { StationTestType.RcPrtt };
+            ExcludeTestTypes = excludetesttypes ?? new List<StationTestType> { StationTestType.Undefined };
+            ServiceLoction = serviceLocation;
+            Serialnumber = serialnumber;
+            ProductNumber = productnumber;
+            RState = rstate;
+            ExcludedRState = excludedrstate;
+            ProductDate = productdate;
+            DateFrom = datefrom;
+            DateTo = dateto;
+        }
+        ~ChooseCase()
+        {
+
+        }
+    }
+}
---
+++ RunSearchTest.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using Ericsson.LogAnalyzer;
+using Ericsson.LogAnalyzer.Models;
+using NSubstitute;
+using NUnit.Framework;
+using System.IO;
+using Ericsson.LogAnalyzer.Definitions;
+using TestUtilities;
+using Ericsson.LogAnalyzer.BLL;
+using System.Linq;
+using System;
+
+namespace LogAnalyzerTest
+{
+    [TestFixture]
+    public class RunSearchTest
+    {
+        public readonly static TestModel TestCase = new TestModel(ProductNumber: "KRC 118*",
+                                                                  searchkeyname: "Linearization_problems, 2/-7, Rev C",
+                                                                  RState: "*",
+                                                                  SerialNumber: "*",
+                                                                  hitsexpected: "1",
+                                                                  // STRING OCCURRENCE PATTERN:
+                                                                  // string_1, string_2, ..., String_N <NEXTFILE> string_1, string_2, ..., string_N
+                                                                  stringoccurrence: "0,0,0,0,0,0,0,0 <NEXTFILE> 0,0,0,0,0,0,0,0",                                                                                                                                    
+                                                                  searchkeygroup: "Radio");
+
+        TestModel CurrentTestModel { get; set; }
+
+        LogSearchLogic Logic { get; set; }
+
+        private string HwLogCriteria { get; } = Path.Combine(FilePaths.RepoRoot,
+                                                @"Datapackets\HWLogCriteria\documents\HwLogCriteria.xml");
+
+        private string LogPath { get; } = Path.Combine(FilePaths.RepoRoot,
+                                          @"CXP9015115-RBS6000\Test\LogAnalyzerTest\LogFiles");
+
+        List<LogSearchExpressionVariableModel> Variables { get; set; }
+
+        readonly string HitsMessage = "Expected number of hits does not match the actual";
+
+        /// <summary>
+        /// A custom number and types of strings, corresponding to a particular searchcase,
+        /// are written within created corresponding logflies, and expected result is tested. 
+        /// </summary>
+        [Test]
+        [TestCase()]
+        public void RunSearchSTest()
+        {
+            //Arrange            
+
+            CurrentTestModel = TestCase;
+            Logic = new LogSearchLogic(DatabaseVariantType.Xml,
+                                       HwLogCriteria,
+                                       GetCustomFunctions(CurrentTestModel.CurrentProduct));
+            var ExpectedSearchKeyHits = Int32.Parse(CurrentTestModel.HitsExpected);
+            GetLogFilesAndStrings();
+            WriteStringsToFile();
+            //Act
+            var HitsActual = GetActualHits();
+            CurrentTestModel.LogFiles.ForEach(x => File.Delete(LogPath + "\\" + x));            
+            //Assert
+            Assert.AreEqual(ExpectedSearchKeyHits, HitsActual,  HitsMessage);
+        }
+
+        CustomFunctions GetCustomFunctions(ProductModel product)
+        {
+            return new CustomFunctions(null, product, Substitute.For<ILogger>());
+        }
+
+        private void GetLogFilesAndStrings()
+        {
+            var ResultModel = Logic.RunSearch(LogPath,
+                    CurrentTestModel.CurrentProduct,
+                    CurrentTestModel.SearchKeyGroup,
+                    CurrentTestModel.TestType,
+                    CurrentTestModel.ServiceLocation);
+
+            CurrentTestModel.LogFiles = ResultModel.LogSearchKeys.
+                Where(x => x.Name == CurrentTestModel.SearchKeyName).First().SearchSettings.IncludedFiles;
+            var AllStrings = ResultModel.LogSearchKeys.
+                Where(x => x.Name == CurrentTestModel.SearchKeyName).First().SearchSettings.Expression.Variables.Select(x => x.Value).ToList();
+            CurrentTestModel.AllStrings = AllStrings;
+        }
+
+        private void WriteStringsToFile()
+        {
+            var AllStrings = CurrentTestModel.AllStrings;
+            var LogFiles = CurrentTestModel.LogFiles;
+            var OccurencesAllFiles = CurrentTestModel.StringOccurrence.Split(new string[] { " <NEXTFILE> " }, StringSplitOptions.None);
+            var col = LogFiles.Zip(OccurencesAllFiles, (x, y) => new { LogFile = x, OccurrencesString = y });
+            foreach (var Entry in col)
+            {
+                if (!File.Exists(LogPath + "\\" + Entry.LogFile))
+                {
+                    var CurrentFile = File.CreateText(LogPath + "\\" + Entry.LogFile);
+                    CurrentFile.Close();
+                }                
+                StreamWriter outputFile = new StreamWriter(Path.Combine(LogPath, Entry.LogFile),true);
+                var Occurrences = Entry.OccurrencesString.Split(',').Select(x => Int32.Parse(x)).ToList();
+                var col2 = AllStrings.Zip(Occurrences, (x, y) => new { Line = x, Occurrence = y });
+                col2.Where(x => x.Occurrence != 0).ToList().
+                     ForEach(x => outputFile.WriteLine(string.Concat(Enumerable.Repeat($"{x.Line}\r\n", x.Occurrence))));
+                outputFile.Close();
+            }
+        }
+        private int GetActualHits()
+        { 
+            var ResultModel = Logic.RunSearch(LogPath,
+                                CurrentTestModel.CurrentProduct,
+                                CurrentTestModel.SearchKeyGroup,
+                                CurrentTestModel.TestType,
+                                CurrentTestModel.ServiceLocation);
+            int Hits = ResultModel.LogSearchKeysHits.Where(x => x.Name == CurrentTestModel.SearchKeyName).Count() > 0 ?
+                       ResultModel.LogSearchKeysHits.Count : 0;
+            return Hits;
+        }
+    }
+
+    public class TestModel
+    {
+        public ProductModel CurrentProduct { get; set; }
+
+        public string SearchKeyGroup { get; set; }
+
+        public string SearchKeyName { get; set; }
+
+        public string StringOccurrence { get; set; }
+
+        public List<string> LogFiles { get; set; }
+
+        public List<string> AllStrings { get; set; }
+
+        public string HitsExpected { get; set; }
+
+        public string ServiceLocation { get; set; }
+
+        public StationTestType TestType { get; set; }
+
+        public TestModel(string ProductNumber,
+                         string searchkeygroup,
+                         string searchkeyname,
+                         string stringoccurrence,                         
+                         string RState = null,
+                         string hitsexpected = null,
+                         string SerialNumber = null,
+                         string DateFrom = null,
+                         string DateTo = null,
+                         string ExcludedRState = null,
+                         string ProductDate = null,
+                         string servicelocation = null,
+                         StationTestType testtype = StationTestType.All)
+        {
+            CurrentProduct = Substitute.For<ProductModel>();
+            CurrentProduct.ProductNumber = ProductNumber;
+            CurrentProduct.RState = RState;
+            CurrentProduct.SerialNumber = SerialNumber;
+            CurrentProduct.DateFrom = DateFrom;
+            CurrentProduct.DateTo = DateTo;
+            CurrentProduct.ProductDate = ProductDate;
+            CurrentProduct.ExcludedRState = ExcludedRState;            
+            SearchKeyGroup = searchkeygroup;
+            SearchKeyName = searchkeyname;
+            StringOccurrence = stringoccurrence;
+            HitsExpected = hitsexpected;
+            TestType = testtype;
+            ServiceLocation = servicelocation;
+        }
+        ~TestModel()
+        {
+
+        }
+    }
+}
