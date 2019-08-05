a776e9e7b37118fceaa22cd05f4815e57fcccf63
jogo <jogo@syntronic.com>
2019-08-05 21:48:07 +0200

    20190805

---
+++ SearchKey_Rep/SearchKey_Rep/Transpose.fs
@@ -0,0 +1,47 @@
+﻿namespace SearchKeyRep
+
+open System
+
+module Transpose =
+                
+    type PLLVar = 
+        {   Value : string
+            file : string
+            }  
+
+    type PLLVars = 
+        {   X1 : PLLVar
+            X2 : PLLVar
+            } 
+        
+    let TransposeStrArr (arr : string[][]) =
+        
+        let arr2Fill = 
+            arr.[0]
+            |> Array.map(fun sub_arr -> [|sub_arr|])
+        
+        let otherArrs = 
+            arr
+            |> Array.skip(1)
+
+        let transposedArr = 
+            Array.zip arr2Fill [|0..arr2Fill.Length - 1|]
+            |> Array.map(fun (sub_arr, index) -> Array.append sub_arr (otherArrs|> Array.map(fun other_sub_arr -> other_sub_arr.[index])))
+                                                                            
+        transposedArr
+
+    let TransposePLLArr (arr : PLLVars[][]) =
+        
+        let arr2Fill = 
+            arr.[0]
+            |> Array.map(fun sub_arr -> [|sub_arr|])
+        
+        let otherArrs = 
+            arr
+            |> Array.skip(1)
+
+        let transposedArr = 
+            Array.zip arr2Fill [|0..arr2Fill.Length - 1|]
+            |> Array.map(fun (sub_arr, index) -> Array.append sub_arr (otherArrs|> Array.map(fun other_sub_arr -> other_sub_arr.[index])))
+                                                                            
+        transposedArr
