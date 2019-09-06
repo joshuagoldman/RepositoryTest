362e84f7206a5fafcddf3c70b7339f2a0c409af3
Joshua Goldman <joshuagoldman94@gmail.com>
2019-09-06 09:21:43 +0200

    Added solution

---
+++ EricssonSupportAssistance/EricssonSupportAssistance/Models.fs
@@ -0,0 +1,20 @@
+﻿namespace EricssonSupportAssistance.Models
+
+open System
+
+    
+type ControllAttributes() = 
+        
+    let mutable text = ""
+    let mutable items_source = [|""|]
+        
+    member this.Text 
+        with get() = text
+        and set(value) =
+            text <- value
+
+    member this.ItemsSource 
+        with get() = items_source
+        and set(value) =
+            items_source <- value
+
---
+++ EricssonSupportAssistance/EricssonSupportAssistance/ViewModels.fs
@@ -0,0 +1,22 @@
+﻿namespace EricssonSupportAssistance.ViewModels
+
+open EricssonSupportAssistance.Models
+ 
+ type SearchTicketViewModel() = 
+     
+     member val SearchPhraseComboBox = new ControllAttributes() with get, set
+     member val AlternativesComboBox = new ControllAttributes() with get, set
+     member val InfoMessage = new ControllAttributes() with get, set
+
+type ShellViewModel() = 
+    
+    member val UpdateButton = new ControllAttributes() with get, set
+    member val LoginButton = new ControllAttributes() with get, set
+    member val UsernameTextBlock = new ControllAttributes() with get, set
+    member val PasswordTextBlock = new ControllAttributes() with get, set
+    member val TicketTextBlock = new ControllAttributes() with get, set
+    member val SolutionTextBlock = new ControllAttributes() with get, set
+    member val SearchTicketButton = new ControllAttributes() with get, set
+    member val InfoMessage = new ControllAttributes() with get, set
+
+
